19336acd2e876d7818942733d00fb5d3d9bd372c
Shinoyu89 <92374891+Shinoyu89@users.noreply.github.com>
2021-12-20 01:30:26 +0900

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,8 @@
+const playBtn = document.querySelector('.video__player-img');
+const video = document.getElementById('player');
+
+video.addEventListener('loadeddata', () ⇒ ｛
+    video.addEventListener('click', () )⇒ {
+        video.play();
+    })
+    
